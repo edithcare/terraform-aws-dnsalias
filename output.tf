a06d740268785a5c5b01ed7ada79b4b324aa9672
Lars Campe <campe@silpion.de>
2020-01-14 14:06:14 +0100

    Fixed typo in output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output record_name {
   value       = aws_route53_record.edith_care.fqdn
-  description = "The FQDN of the resultiung DNS record."
+  description = "The FQDN of the resulting DNS record."
 }
