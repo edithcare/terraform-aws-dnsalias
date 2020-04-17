c6cb567cc6af046b5601de6a68194b28e5eaaf13
Michael Gröning <groening@silpion.de>
2020-04-17 10:41:11 +0200

    removed bucket_arn output

--- output.tf
+++ output.tf
@@ -2,8 +2,3 @@ output record_name {
   value       = aws_route53_record.record.fqdn
   description = "The FQDN of the resulting DNS record."
 }
-
-output bucket_arn {
-  value       = aws_s3_bucket.bucket_index[0].arn
-  description = "The arn of the s3-bucket."
-}
